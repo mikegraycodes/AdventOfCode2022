d2fa374e273ac582b931de5084f39018f6682a63
Michael Gray <michael.gray@clear.bank>
2022-12-04 15:04:38 +0000

    day 3 and 4

---
+++ AdventOfCode2022/Day3/Program.cs
@@ -0,0 +1,61 @@
+﻿var ruckSacks = new List<Rucksack>();
+
+File.ReadLines(@"input.txt").ToList().
+    ForEach(line => ruckSacks.Add(new Rucksack(line[..(line.Length/2)], line[(line.Length/2)..])));
+
+
+var elveGroups = new List<ElvesGroup>();
+
+ruckSacks.Chunk(3).ToList().ForEach(x => elveGroups.Add(new ElvesGroup(x.ToList())));
+
+
+Console.WriteLine($"Part 1: {ruckSacks.Sum(x => x.ItemPriorityTotal)}");
+Console.WriteLine($"Part 2: {elveGroups.Sum(x => x.ItemPriorityTotal)}");
+
+
+
+public class ElvesGroup
+{
+    private readonly List<Rucksack> _ruckSacks;
+
+    public int ItemPriorityTotal => ItemPriority(_ruckSacks[0].BothCompartements.Select(x => x).Where(x => x.ToString().Intersect(_ruckSacks[1].BothCompartements).Any() && x.ToString().Intersect(_ruckSacks[2].BothCompartements).Any()).First());
+
+    public ElvesGroup(List<Rucksack> ruckSacks)
+    {
+        if (ruckSacks.Count != 3) throw new Exception("Elves must be in groups of 3");
+
+        _ruckSacks = ruckSacks;
+    }
+    private int ItemPriority(char input)
+    {
+        return char.IsUpper(input) ? input - 38 : input - 96;
+    }
+}
+
+
+
+public class Rucksack
+{
+    private readonly string _compartement1;
+    private readonly string _compartement2;
+
+    public string BothCompartements => _compartement1 + _compartement2;
+
+    public int ItemPriorityTotal => ItemPriority(_compartement1.Select(x => x).Where(x => _compartement2.Contains(x)).First());
+
+
+    public Rucksack(string compartement1, string compartement2)
+    {
+        _compartement1 = compartement1;
+        _compartement2 = compartement2;
+    }
+
+    private int ItemPriority(char input)
+    {
+        return char.IsUpper(input) ? input - 38 : input - 96;
+    }
+
+}
+
+
+
---
+++ AdventOfCode2022/Day4/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var ElfAssignments = new List<ElfAssignments>();
+
+File.ReadLines(@"input.txt").ToList()
+    .ForEach(x => ElfAssignments
+    .Add(new ElfAssignments(new Assignment(int.Parse(x.Split(",")[0].Split("-")[0]), int.Parse(x.Split(",")[0].Split("-")[1])),
+                            new Assignment(int.Parse(x.Split(",")[1].Split("-")[0]), int.Parse(x.Split(",")[1].Split("-")[1])))));
+
+Console.WriteLine(ElfAssignments.Count(x => x.CompletelyOverlap));
+Console.WriteLine(ElfAssignments.Count(x => x.SomewhatOverlap));
+
+
+public class ElfAssignments
+{
+    private readonly Assignment _assignment1;
+    private readonly Assignment _assignment2;
+
+    public bool CompletelyOverlap => _assignment1.Numbers.All(x => _assignment2.Numbers.Contains(x)) ||
+                                     _assignment2.Numbers.All(x => _assignment1.Numbers.Contains(x));
+
+    public bool SomewhatOverlap => _assignment1.Numbers.Intersect(_assignment2.Numbers).Any();
+
+    public ElfAssignments(Assignment assignment1, Assignment assignment2)
+    {
+        _assignment1 = assignment1;
+        _assignment2 = assignment2;
+    }
+ }
+
+public class Assignment
+{
+    public int Lower { get; }
+    public int Upper { get; }
+
+    public IEnumerable<int> Numbers => Enumerable.Range(Lower, Upper - Lower + 1);
+
+    public Assignment(int lower, int upper)
+    {
+        Lower = lower;
+        Upper = upper;
+    }
+}
+    
